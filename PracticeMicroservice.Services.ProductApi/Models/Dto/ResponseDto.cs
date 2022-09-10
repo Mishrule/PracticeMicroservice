a71fc658d1058b2b77515f27b8e76c82ee9e2346
Mensah <Mensah@DESKTOP-29Q93C5>
2022-09-10 22:05:00 +0000

    wip

---
+++ PracticeMicroservice.Services.ProductApi/Models/Dto/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticeMicroservice.Services.ProductApi.Models.Dto
+{
+  public class ResponseDto
+  {
+    public bool IsSuccess { get; set; } = true;
+    public object Result { get; set; }
+    public string DisplayMessage { get; set; } = string.Empty;
+    public List<string> ErrorsMessage { get; set; } = new List<string>();
+  }
+}
---
+++ PracticeMicroservice.Services.ProductApi/Repository/ProductRepository.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PracticeMicroservice.Services.ProductApi.DbContexts;
+using PracticeMicroservice.Services.ProductApi.Models;
+using PracticeMicroservice.Services.ProductApi.Models.Dto;
+
+namespace PracticeMicroservice.Services.ProductApi.Repository
+{
+  public class ProductRepository : IProductRepository
+  {
+    private readonly ApplicationDbContext _db;
+    private IMapper _mapper;
+    public ProductRepository(ApplicationDbContext applicationDbContext, IMapper mapper)
+    {
+      _db = applicationDbContext;
+      _mapper = mapper;
+    }
+
+    public async Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+    {
+      Product product = _mapper.Map<ProductDto, Product>(productDto);
+      if (product.ProductId > 0)
+      {
+        _db.Products.Update(product);
+      }
+      else
+      {
+        _db.Products.Add(product);
+      }
+      await _db.SaveChangesAsync();
+      return _mapper.Map<Product, ProductDto>(product);
+    }
+
+    public async Task<bool> DeleteProduct(int id)
+    {
+      try
+      {
+        Product product = await _db.Products.FirstOrDefaultAsync(p => p.ProductId == id);
+        if (product == null)
+        {
+          return false;
+        }
+        _db.Products.Remove(product);
+        return true;
+      }
+      catch (Exception)
+      {
+
+        return false;
+      }
+    }
+
+    public async Task<ProductDto> GetProductById(int id)
+    {
+      Product product = await _db.Products.Where(x => x.ProductId == id).FirstOrDefaultAsync();
+      return _mapper.Map<ProductDto>(product);
+    }
+
+    public async Task<IEnumerable<ProductDto>> GetProducts()
+    {
+      List<Product> productList = await _db.Products.ToListAsync();
+      return _mapper.Map<List<ProductDto>>(productList);
+    }
+  }
+}
