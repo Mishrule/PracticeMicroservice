88e419734f8d6cea8d2c38e56828180c1ab47afd
Ernest Kyei <eknkrumah1@umat.edu.gh>
2022-09-15 17:05:19 +0000

    wip

---
+++ PracticeMicroservice.Services.Identity/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PracticeMicroservice.Services.Identity.Models;
+
+namespace PracticeMicroservice.Services.Identity.DbContext
+{
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
+  }
+}
---
+++ PracticeMicroservice.Services.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PracticeMicroservice.Services.Identity.Models
+{
+  public class ApplicationUser:IdentityUser
+  {
+  }
+}
